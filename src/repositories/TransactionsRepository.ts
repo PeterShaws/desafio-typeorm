87a95e2f454aa5d0190c0882aa76a54ca6368d94
Pedro A. C. Jr <pchavesjr@gmail.com>
2020-11-07 12:28:15 -0300

    feature: List transactions and calculate balance
    - Add `--poll` flag to `ts-node-dev`
      - (as needed by Node.js 14 on the Linux platform)
    - ESLint
      - Broaden the pattern for ignoring unused arguments
      - Allow the use of `console`
    - TypeORM
      - Generate migration for creating "transactions" table
      - Generate migration for creating "categories" table
    - Annotate models
    - Implement `TransactionsRepository`’s `getBalance()` method

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -8,10 +8,26 @@ interface Balance {
   total: number;
 }
 
+type TransactionType = 'income' | 'outcome';
+
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+    const income = this.getTotalForType(transactions, 'income');
+    const outcome = this.getTotalForType(transactions, 'outcome');
+    const total = income - outcome;
+    return { income, outcome, total };
+  }
+
+  private getTotalForType(
+    transactions: Transaction[],
+    type: TransactionType,
+  ): number {
+    return transactions
+      .filter(transaction => transaction.type === type)
+      .map(transaction => transaction.value)
+      .reduce((total, value) => total + value, 0);
   }
 }
 
